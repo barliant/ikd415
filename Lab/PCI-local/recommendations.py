5ee02ad9a5de95a660041a74eb1e3130f4176c2d
Anung Mint Rosa <barliant@gmail.com>
2017-10-10 08:43:54 +0700

    Add local modifications of recommendations.py

--- Lab/PCI-local/recommendations.py
+++ Lab/PCI-local/recommendations.py
@@ -26,10 +26,12 @@ def sim_distance(prefs,person1,person2):
   # Get the list of shared_items
   si={}
   for item in prefs[person1]: 
-    if item in prefs[person2]: si[item]=1
+    if item in prefs[person2]:
+      si[item]=1
 
   # if they have no ratings in common, return 0
-  if len(si)==0: return 0
+  if len(si)==0:
+    return 0
 
   # Add up the squares of all the differences
   sum_of_squares=sum([pow(prefs[person1][item]-prefs[person2][item],2) 
@@ -42,11 +44,13 @@ def sim_distance(prefs,person1,person2):
 def sim_pearson(prefs,p1,p2):
   # Get the list of mutually rated items
   si={}
-  for item in prefs[p1]: 
-    if item in prefs[p2]: si[item]=1
+  for item in prefs[p1]:
+    if item in prefs[p2]:
+      si[item]=1
 
   # if they are no ratings in common, return 0
-  if len(si)==0: return 0
+  if len(si)==0:
+    return 0
 
   # Sum calculations
   n=len(si)
@@ -65,7 +69,8 @@ def sim_pearson(prefs,p1,p2):
   # Calculate r (Pearson score)
   num=pSum-(sum1*sum2/n)
   den=sqrt((sum1Sq-pow(sum1,2)/n)*(sum2Sq-pow(sum2,2)/n))
-  if den==0: return 0
+  if den==0:
+    return 0
 
   r=num/den
 
@@ -87,11 +92,13 @@ def getRecommendations(prefs,person,similarity=sim_pearson):
   simSums={}
   for other in prefs:
     # don't compare me to myself
-    if other==person: continue
+    if other==person:
+      continue
     sim=similarity(prefs,person,other)
 
     # ignore scores of zero or lower
-    if sim<=0: continue
+    if sim<=0:
+      continue
     for item in prefs[other]:
 	    
       # only score movies I haven't seen yet
@@ -132,7 +139,8 @@ def calculateSimilarItems(prefs,n=10):
   for item in itemPrefs:
     # Status updates for large datasets
     c+=1
-    if c%100==0: print "%d / %d" % (c,len(itemPrefs))
+    if c%100==0:
+      print "%d / %d" % (c,len(itemPrefs))
     # Find the most similar items to this one
     scores=topMatches(itemPrefs,item,n=n,similarity=sim_distance)
     result[item]=scores
@@ -149,7 +157,8 @@ def getRecommendedItems(prefs,itemMatch,user):
     for (similarity,item2) in itemMatch[item]:
 
       # Ignore if this user has already rated this item
-      if item2 in userRatings: continue
+      if item2 in userRatings:
+        continue
       # Weighted sum of rating times similarity
       scores.setdefault(item2,0)
       scores[item2]+=similarity*rating
